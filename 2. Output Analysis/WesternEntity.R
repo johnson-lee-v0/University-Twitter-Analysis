82213f85d3bd3dc5fa7681ef6f49295d3016a1f8
johnson-lee-v0 <89353467+johnson-lee-v0@users.noreply.github.com>
2022-04-25 13:15:02 -0400

    Add files via upload

---
+++ 2. Output Analysis/WesternEntity.R
@@ -0,0 +1,19 @@
+library(dplyr)
+Data=read.csv('WesternTweetsResult.csv',header = T)
+
+#Date Entity
+Date=Data%>%filter(Data$Type=="Date")
+DateResult=Date[!grepl("://", Date$Entity),]
+DateResult%>%count(DateResult$Entity)%>% arrange(desc(n))
+#Location
+Location=Data%>%filter(Data$Type=="Location")
+Location%>%count(Location$Entity)%>% arrange(desc(n))
+#Money
+Money=Data%>%filter(Data$Type=="Money")
+Money%>%count(Money$Entity)%>% arrange(desc(n))
+#Organization
+Organization=Data%>%filter(Data$Type=="Organization")
+Organization%>%count(Organization$Entity)%>% arrange(desc(n))
+#Person
+Person=Data%>%filter(Data$Type=="Person")
+Person%>%count(Person$Entity)%>% arrange(desc(n))
